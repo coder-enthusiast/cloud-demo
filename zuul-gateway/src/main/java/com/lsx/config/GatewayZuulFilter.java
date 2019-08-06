942a53906806379b5e2560eb20817201296c35ef
lsx <1163557638@qq.com>
2019-08-06 21:58:45 +0800

    添加zuul网关校验测试。

--- zuul-gateway/src/main/java/com/lsx/config/GatewayZuulFilter.java
+++ zuul-gateway/src/main/java/com/lsx/config/GatewayZuulFilter.java
@@ -1,8 +1,15 @@
 package com.lsx.config;
 
 import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.http.HttpStatus;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
 import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
 
 @Component
 public class GatewayZuulFilter extends ZuulFilter {
@@ -15,7 +22,7 @@ public class GatewayZuulFilter extends ZuulFilter {
      */
     @Override
     public String filterType() {
-        return "pre";
+        return FilterConstants.PRE_TYPE;
     }
 
     /**
@@ -23,20 +30,32 @@ public class GatewayZuulFilter extends ZuulFilter {
      */
     @Override
     public int filterOrder() {
-        return 0;
+        return FilterConstants.PRE_DECORATION_FILTER_ORDER - 1;
     }
     /**
      * 这里可以写逻辑判断，是否要过滤，本文true,永远过滤。
      */
     @Override
     public boolean shouldFilter() {
-        return false;
+        return true;
     }
     /**
      * 过滤器的具体逻辑。可用很复杂，包括查sql，nosql去判断该请求到底有没有权限访问。
      */
     @Override
     public Object run() throws ZuulException {
+        //获取请求上下文
+        RequestContext ctx=RequestContext.getCurrentContext();
+        //获取request
+        HttpServletRequest request=ctx.getRequest();
+        //获取请求参数token
+        String token=request.getParameter("access-token");
+        //判断是否存在
+        if(StringUtils.isBlank(token)){
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(HttpStatus.SC_FORBIDDEN);
+        }
+
         return null;
     }
 }
